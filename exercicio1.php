79d75c7ceccccac88cb948908bb99bde55fbd2a5
Suhailah de Moraes <68478385+SuhMoraes@users.noreply.github.com>
2020-10-21 16:58:09 -0300

    Exercício 1 - Recode Pro

---
+++ exercicio1.php
@@ -0,0 +1,18 @@
+<?php
+    $titulo = "Ser ou não um ótimo Programador Full Stack?!";
+    $descricao = "Saber bem de todas as linguagens propostas no curso.";
+    $status = "Para ser a melhor no que propuser a fazer.";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title> <?php echo $titulo; ?> </title>
+    <body>
+        <p> <?php echo $descricao; ?> </p>
+        <p> <b> <?php echo $status; ?></p></b>
+    </body>
+
+    </head>
